d29251e44fb151a87157e217d6be01684c6489e0
gurgelaf <andnux0@gmail.com>
2020-03-17 03:00:24 -0300

    Updates

---
+++ precache-manifest.2832e72bbcf14267a734db488f6e37a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7462ad59858b1e28eadad7429b353e95",
+    "url": "/pizzaria/index.html"
+  },
+  {
+    "revision": "7061dc33549886f19793",
+    "url": "/pizzaria/static/css/main.69cd0dde.chunk.css"
+  },
+  {
+    "revision": "278d976c3e93e5be67e0",
+    "url": "/pizzaria/static/js/2.b9ee165a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pizzaria/static/js/2.b9ee165a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7061dc33549886f19793",
+    "url": "/pizzaria/static/js/main.915ec067.chunk.js"
+  },
+  {
+    "revision": "457ca4455a23ceeb0c55",
+    "url": "/pizzaria/static/js/runtime-main.40edde8b.js"
+  },
+  {
+    "revision": "9a1d1aa461f75bd32aa1828f9589400a",
+    "url": "/pizzaria/static/media/casa.9a1d1aa4.jpg"
+  },
+  {
+    "revision": "e5067bf99c573422aa466f571bf1dac2",
+    "url": "/pizzaria/static/media/emprego.e5067bf9.jpg"
+  },
+  {
+    "revision": "a5972ca5edb2f8853890d3dbbd018120",
+    "url": "/pizzaria/static/media/peru.a5972ca5.png"
+  },
+  {
+    "revision": "09e062b5c9c4f0785769cc57b6a9a84b",
+    "url": "/pizzaria/static/media/pizza.09e062b5.jpg"
+  }
+]);
